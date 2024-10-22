4fbcfd5c7e122ec3e47155142f2e05ba934123fb
Stephen Punak <steve@punak.com>
2024-10-22 00:00:11 +0000

    Icon label widget

---
+++ include/Kanoop/gui/widgets/iconlabel.h
@@ -0,0 +1,35 @@
+#ifndef ICONLABEL_H
+#define ICONLABEL_H
+
+#include <QWidget>
+
+class QLabel;
+class IconLabel : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit IconLabel(QWidget *parent = nullptr);
+    explicit IconLabel(const QString& text, QWidget *parent = nullptr);
+    explicit IconLabel(const QString& text, const QIcon& icon, QWidget *parent = nullptr);
+
+    QString text() const;
+    void setText(const QString& text);
+
+    QIcon icon() const;
+    void setIcon(const QIcon& icon);
+
+    void setIconAlignment(Qt::Alignment alignment);
+
+private:
+    void createLayout(const QString& text, const QIcon& icon);
+
+    QLabel* _label = nullptr;
+    QLabel* _iconLabel = nullptr;
+
+signals:
+
+private slots:
+
+};
+
+#endif // ICONLABEL_H
