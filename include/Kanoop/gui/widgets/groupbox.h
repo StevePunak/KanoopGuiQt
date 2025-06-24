42a9415bf926a844aa67a1c67e359cb495b3730d
Stephen Punak <stephen.punak@epcpower.com>
2025-06-24 09:56:41 -0700

    Add GroupBox subclass

---
+++ include/Kanoop/gui/widgets/groupbox.h
@@ -0,0 +1,20 @@
+#ifndef GROUPBOX_H
+#define GROUPBOX_H
+
+#include <QGroupBox>
+
+#include <Kanoop/utility/loggingbaseclass.h>
+
+class GroupBox : public QGroupBox,
+                 public LoggingBaseClass
+{
+    Q_OBJECT
+public:
+    explicit GroupBox(QWidget *parent = nullptr);
+    explicit GroupBox(const QString& title, QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // GROUPBOX_H
