eff89330bbeb5cde1dc3d86784127ed86622ae0e
Stephen Punak <stephen.punak@epcpower.com>
2026-04-25 15:11:27 -0700

    Fix checkbox plugin

--- src/gui/widgets/checkboxdesignerplugin.h
+++ src/gui/widgets/checkboxdesignerplugin.h
@@ -16,7 +16,7 @@ public:
     virtual QString group() const override { return "Kanoop"; }
     virtual QString toolTip() const override { return "Check Box Widget"; }
     virtual QString whatsThis() const override { return "Check Box Widget"; }
-    virtual QString includeFile() const override { return "Kanoop/gui/widgets/xxxcheckbox.h"; }
+    virtual QString includeFile() const override { return "Kanoop/gui/widgets/checkbox.h"; }
     virtual QIcon icon() const override;
     virtual bool isContainer() const override { return false; }
     virtual QWidget* createWidget(QWidget* parent) override;
